a3b0187e08254e4e7a2c201f4b228f3389ec6b34
David Rodriguez <ezequiel-07@hotmai.com>
2022-09-30 15:21:33 -0300

    Ejercicio_leccion_2.6

---
+++ Ejercicio_leccion_2.6.js
@@ -0,0 +1,12 @@
+const misDatos = [
+    "David", 
+    31, 
+    false, 
+    new Date("july 02 1991"),
+    Libros_fav = {
+        titulo: "El principito",
+        autor: "Antoine de Saint-Exupéry",
+        fecha: new Date(1943),
+        url: "https://es.wikipedia.org/wiki/Antoine_de_Saint-Exup%C3%A9ry"
+    }
+]    
